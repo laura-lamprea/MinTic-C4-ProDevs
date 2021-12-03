9b648e53bc0d08a71554cc8ea65b29e29eb89a9e
Laura <laura.lampream@gmail.com>
2021-12-03 17:31:00 -0500

    Helper-middleware para implem. jwt, resolvers schema de usuario

--- back/src/graphql/resolvers.js
+++ back/src/graphql/resolvers.js
@@ -1,45 +1,54 @@
 import bcryp from "bcrypt";
 //import Curso from "../models/Curso";
 import Proyectos from "../models/Proyectos";
-//import Usuarios from "../models/Usuarios";
+import Usuarios from "../models/Usuarios";
 //import Avances from "../models/Avances";
 //import Inscripciones from "../models/Inscripciones";
-
+import { generarJwt } from "../helpers/jwt";
 
 export const resolvers = {
     Query : {
         Proyectos() {
-            return Proyecto.find();
+            return Proyectos.find();
         },
-        // async Login(_, {email, password}) {
-            
-        //     const usuario = await Usuario.findOne({
-        //         email
-        //     })
-        //     if (!usuario){
-        //         return "Usuario o contraseña incorrecto";
-        //     }
-            
-        //     const validarPassword = bcryp.compareSync(password, usuario.password)
-        //     if (validarPassword){
-        //         return "Exitoso";
-        //     }
-        //     else {
-        //         return "Usuario o contraseña incorrecto";
-        //     }
-        // }
+        Usuarios() {
+            return Usuarios.find();
+        },
+        async Login(_, {email, password}) {
+            const usuario = await Usuarios.findOne({
+                email
+            })
+            if (!usuario){
+                return "Usuario o contraseña incorrecto";
+            }
+            const validarPassword = bcryp.compareSync(password, usuario.password)
+            if (validarPassword){
+                const token = await generarJwt(usuario.id, usuario.nombre)
+                //console.log("Login exitoso")
+                return token;
+            }
+            else {
+                return "Usuario o contraseña incorrecto";
+            }
+        }
     },
+
+
     Mutation : {
-        async agregarProyecto(_, {proyecto}){
-            const unProyecto = new Proyectos(proyecto);
+        async addProject(_, {project}){
+            const unProyecto = new Proyectos(project);
             return await unProyecto.save();
         },
 
-        // async AgregarUsuario(_, { usuario }) {            
-        //     const salt = bcryp.genSaltSync();
-        //     let nUsuario = new Usuario(usuario);
-        //     nUsuario.password = bcryp.hashSync(usuario.password, salt);
-        //     return await nUsuario.save();
-        // }
+        async updateProject(_, {id}){
+            return await Proyectos.findByIdAndUpdate(id);
+        },
+       
+        async addUser(_, {user}) {            
+            const salt = bcryp.genSaltSync();
+            let unUsuario = new Usuarios(user);
+            unUsuario.password = bcryp.hashSync(user.password, salt);
+            return await unUsuario.save();
+        }
     }
 }
--- back/src/models/Proyectos.js
+++ back/src/models/Proyectos.js
@@ -3,61 +3,57 @@ const { Schema, model } = require('mongoose');
 const ProyectoSchema = Schema({
     id_project: {
         type: String,
-        required: true,
+        //required: true,
         unique: true
     },
 
     project: {
         type: String,
-        required: true,
+        //required: true,
         unique: true
     },
     
-    general_objectives: {
+    general_objectives:{
         type: String,
-        required: true
     },
 
-    specific_objectives: {
-        type: String,
-        required: true
-    },
+    specific_objectives: [{
+        item:String
+    }],
 
     budget: {
         type: String,
-        required: true,
+        //required: true,
     },
 
     date_start: {
         type: String,
-        required: true,
+       // required: true,
     },
 
     date_finish: {
         type: String,
-        required: true,
+        //required: true,
     },
 
     id_user: {
         type: String,
-        required: true,
+       //required: true,
         unique: true
     },
     
     user: {
         type: String,
-        required: true
+        //required: true
     },
 
     state_project: {
         type: String,
-        required: true,
         default: 'Inactivo'
     },
 
     phase: {
         type: String,
-        required: true,
         default: 'Nula'
     },
 
