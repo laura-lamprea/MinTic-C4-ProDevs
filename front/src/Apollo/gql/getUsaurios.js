2ba0da34ff99b42549f5c1216181f8fec5087a1c
dmorenro <diegoxfy@gmail.com>
2021-12-12 13:41:58 -0500

    Se navega entre mensu (Registro/Usuario/Proyectos)

--- front/src/Apollo/gql/getUsaurios.js
+++ front/src/Apollo/gql/getUsaurios.js
@@ -1,12 +1,14 @@
 import { gql } from '@apollo/client'
 
-const GET_USUARIOS = gql `
-    
-    Executy Query
-    query {
+const GET_USUARIOS = gql`
+        query {            
+            usuarios {
+                id
+                nombre
+                apellido
+                email
+            }               
+        }
+    `;
 
-        
-    }
-
-
-`
+export default GET_USUARIOS;
--- front/src/Components/usuarios/UsuarioPage.js
+++ front/src/Components/usuarios/UsuarioPage.js
@@ -1,61 +1,48 @@
-// import React from 'react'
-// import { NavLink, useParams } from 'react-router-dom';
-// //import '../components.css';
-// import CrearUsuario from './crear/CrearUsuario';
-// import EditarUsuario from './editar/EditarUsuario';
-// import ListarUsuarios from './listar/ListarUsuarios';
-
-// const UsuariosPage = () => {
-
-//     const { action } = useParams();
-
-//     console.log(action);
-
-//     return (
-//         <section className="content mt-5">
-//             <div className="container-fluid">
-//                 <div className="row">
-//                     <div className="col-lg-12">
-//                         <div className="card card-primary card-outline">
-//                             <div className="card-header flex">
-//                                 <h5 className="m-0">Usuarios</h5>
-//                                 {
-//                                     action !== undefined ?
-
-//                                         <NavLink className="btn btn-danger mr-3" to={`/usuarios`}>
-//                                             Cancelar
-//                                         </NavLink>
-//                                         :
-//                                         <NavLink className="btn btn-primary mr-3" to={`/usuarios/crear`}>
-//                                             Crear Producto
-//                                         </NavLink>
-
-//                                 }
-//                             </div>
-//                             <div className="card-body">
-//                                 {action === '' || action === undefined ? <ListarUsuarios /> : (action === 'crear') ? <CrearUsuario /> : <EditarUsuario />}
-//                             </div>
-//                         </div>
-//                     </div>
-//                 </div>
-//             </div>
-//         </section>
-//     )
-// }
-
-// export default UsuariosPage
-
-
-//---------------
-
-
 import React from 'react'
+import { NavLink, useParams } from 'react-router-dom';
+//import '../components.css';
 
-export const UsuarioPage = () => {
+import CrearUsuario from './crear/CrearUsuario'
+import EditarUsuario from './editar/EditarUsuario';
+import ListarUsuarios from './listar/ListarUsuarios';
+
+const UsuariosPage = () => {
+
+    const { action } = useParams();
+
+    console.log(action);
+
     return (
-        <div>
-            Usuario Page
-        </div>
+        <section className="content mt-5">
+            <div className="container-fluid">
+                <div className="row">
+                    <div className="col-lg-12">
+                        <div className="card card-primary card-outline">
+                            <div className="card-header flex">
+                                <h5 className="m-0">Usuarios</h5>
+                                {
+                                    action !== undefined ?
+
+                                        <NavLink className="btn btn-danger mr-3" to={`/usuarios`}>
+                                            Cancelar
+                                        </NavLink>
+                                        :
+                                        <NavLink className="btn btn-primary mr-3" to={`/usuarios/crear`}>
+                                            Crear Usuario
+                                        </NavLink>
+
+                                }
+                            </div>
+                            <div className="card-body">
+                                {action === '' || action === undefined ? <ListarUsuarios /> : (action === 'crear') ? <CrearUsuario /> : <EditarUsuario />}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
     )
 }
-export default UsuarioPage
+
+export default UsuariosPage
+
